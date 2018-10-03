077348f5b4faf966b86b8cd60ddbde2ce267042e
Fabian Diet <fabian.diet@spaceapplications.com>
2018-10-03 14:43:48 +0200

    Fix super warning

--- yamcs-cli/yamcs/cli/__main__.py
+++ yamcs-cli/yamcs/cli/__main__.py
@@ -10,7 +10,7 @@ from yamcs.cli import clients, instances, links, processors, storage
 class SubCommandHelpFormatter(argparse.RawDescriptionHelpFormatter):
     def _format_action(self, action):
         # Removes the subparsers metavar from the help output
-        parts = super(argparse.RawDescriptionHelpFormatter, self)._format_action(action)
+        parts = super(SubCommandHelpFormatter, self)._format_action(action)
         if action.nargs == argparse.PARSER:
             parts = '\n'.join(parts.split('\n')[1:])
         return parts
@@ -36,31 +36,31 @@ def main():
     clients_parser = subparsers.add_parser('clients', help='List clients')
     clients.configure_parser(clients_parser)
 
-    config_parser = subparsers.add_parser('config',
-                                          help='Manage Yamcs client properties')
+    config_parser = subparsers.add_parser(
+        'config', help='Manage Yamcs client properties')
 
-    instances_parser = subparsers.add_parser('instances',
-                                             help='Read Yamcs instances')
+    instances_parser = subparsers.add_parser(
+        'instances', help='Read Yamcs instances')
     instances.configure_parser(instances_parser)
 
-    links_parser = subparsers.add_parser('links',
-                                         help='Read and manipulate data links')
+    links_parser = subparsers.add_parser(
+        'links', help='Read and manipulate data links')
     links.configure_parser(links_parser)
 
-    processors_parser = subparsers.add_parser('processors',
-                                              help='Read processors')
+    processors_parser = subparsers.add_parser(
+        'processors', help='Read processors')
     processors.configure_parser(processors_parser)
 
-    services_parser = subparsers.add_parser('services',
-                                            help='Read and manipulate services')
+    services_parser = subparsers.add_parser(
+        'services', help='Read and manipulate services')
     services_parser.set_defaults(func=services)
 
-    storage_parser = subparsers.add_parser('storage',
-                                           help='Manage object storage')
+    storage_parser = subparsers.add_parser(
+        'storage', help='Manage object storage')
     storage.configure_parser(storage_parser)
 
-    tables_parser = subparsers.add_parser('tables',
-                                          help='Read and manipulate tables')
+    tables_parser = subparsers.add_parser(
+        'tables', help='Read and manipulate tables')
 
     args = parser.parse_args()
     args.func(args)
