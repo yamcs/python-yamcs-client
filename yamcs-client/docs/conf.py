3d730f4d2912017dac737c0b53787fb6e9b9508b
Fabian Diet <fabian.diet@spaceapplications.com>
2019-05-16 15:59:47 +0200

    Show full toc

--- yamcs-client/docs/conf.py
+++ yamcs-client/docs/conf.py
@@ -46,6 +46,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.doctest',
     'sphinx.ext.intersphinx',
+    'sphinxcontrib.fulltoc',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
